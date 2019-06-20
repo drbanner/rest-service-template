6d39e2ab035b104cf276596da61db6bae29e96d1
Sai Prashanth <sai.alekanti@gmail.com>
2019-06-20 19:41:46 -0400

    validation annotatons

--- src/main/java/com/rest/template/dto/Person.java
+++ src/main/java/com/rest/template/dto/Person.java
@@ -1,14 +1,24 @@
 package com.rest.template.dto;
 
 
+import javax.validation.constraints.*;
+
 public class Person {
 
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 20)
     private String name;
+
+    @Min(0)
+    @Max(100)
     private Integer age;
+
     private Address address;
 
     public Person() {
     }
+
     public Person(String name, Integer age, Address address) {
         this.name = name;
         this.age = age;
